dc4fd47ebc09b6bb8fdc30528b6250f66592e33f
Omar Juma <Omar.Juma@MultiScaleHN.Com>
2017-03-22 11:11:46 -0700

    Fixed bugs

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,22 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
 
-	switch ( node.nodeType )  
+	var child, next;
+
+    if (node.nodeName.toLowerCase() == 'input' || node.nodeName.toLowerCase() == 'textarea'
+        || (node.classList && node.classList.contains('ace_editor'))) { return; }
+
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,15 +30,15 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bIntel\b/g, "NSA");
+	v = v.replace(/\bINTEL\b/g, "NSA");
+	v = v.replace(/\bintel\b/g, "NSA");
+	v = v.replace(/\bIntel®\b/g, "NSA®");
+
 	textNode.nodeValue = v;
 }
 
